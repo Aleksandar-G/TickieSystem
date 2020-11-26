abf92e25f9e3e1a01ba3a353951bfefbf3d62c5e
Georgiev,Aleksandar A.N <aleksandar.georgiev@student.fontys.nl>
2020-11-26 17:57:50 +0100

    render tests

---
+++ ReactApp/src/__tests__/ticket.test.js
@@ -0,0 +1,29 @@
+import {render, cleanup, fireEvent } from "@testing-library/react";
+import * as React from "react";
+import HomeTicket from '../components/HomeTicket';
+
+const ticket = 
+{
+    
+    id: "1",
+    duedate : "20.12.2020",
+    description : "mockdata",
+    difficulty : "Junior",
+    priority : "High"
+    
+};
+
+
+afterEach(cleanup);
+
+test('should render the ticket with the test data', () => {
+    
+
+    const {getByText, getByLabelText, getByTestId} = render(<HomeTicket ticket={ticket}></HomeTicket>)
+
+    getByText("DueDate: 20.12.2020");
+    getByText("PriorityLevel: High");
+    getByText("DifficultyLevel: Junior");
+    getByText("mockdata"); 
+    
+})
