7171a8ffa2274c1e998da6b010ebb385f1b2cd72
Georgiev,Aleksandar A.N <aleksandar.georgiev@student.fontys.nl>
2020-12-03 20:33:19 +0100

    minor fixes

--- ReactApp/src/components/Profile.jsx
+++ ReactApp/src/components/Profile.jsx
@@ -1,39 +1,6 @@
 import axios from "axios";
 import AdminProfile from './AdminProfile';
 import NormalProfile from './NormalProfile';
-
-/*class Profile extends React.Component {
-
-  constructor(props) {
-    super();
-
-    this.state = {
-        tickets: []
-    };
-  }
-
-      componentDidMount(){
-        if(props.choose == "user")
-        axios.get('http://localhost:8080/db/tickets?username='+sessionStorage.getItem("authenticatedUser")).then((res) =>  {this.setState({tickets: res.data})})
-      }
-
-    render() {
-      <Grid container spacing={2}></Grid>
-        let tickets = [];
-        console.log("state"+this.state.tickets.length);
-        //tickets.push(<Grid container spacing={2}>);
-        for (let index = 0; index < this.state.tickets.length; index++) {
-          console.log(this.state.tickets[index])
-          tickets.push(<Grid item xs = {6} sm = {4}  md = {3}><Card ticket= {this.state.tickets[index]}></Card> </Grid>);
-        }
-        
-            return tickets;
-
-    }
-}
-
-export default Profile;*/
-
 import React, { Component } from 'react';
 import ArrangeTickets from "./ArrangeTickets";
 
--- ReactApp/src/components/Ticket.jsx
+++ ReactApp/src/components/Ticket.jsx
@@ -1,77 +1,7 @@
 import React from 'react';
 import UserTicket from './UserTicket';
 import HomeTicket from './HomeTicket';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import AssignTicketService from '../Service/AssignTicketService'
-import axios from 'axios';
 
-/*const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-
-});
-
-
-const doit = function(ticketId,username){
-  axios.post( 'http://localhost:8080/db/tickets/assign', {
-    ticketId: ticketId,
-    username: username
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
-
-export default function Ticket(props) {
-  const classes = useStyles();
-
-  return (
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {props.ticket.id}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          DueDate: {props.ticket.duedate}
-        </Typography>
-        <Typography variant="body2" component="p">
-          PriorotyLevel: <b>{props.ticket.priority}</b>
-          <br />
-          DifficultyLevel: <b>{props.ticket.difficulty}</b>
-        </Typography>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          {props.ticket.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-       
-        <Button size="small" onClick={() => doit(props.ticket.id,sessionStorage.getItem("authenticatedUser"))}>Assign Ticket</Button>
-      </CardActions>
-    </Card>
-  );
-}
-*/
-//onClick={AssignTicketService.assignTicket(props.ticket.id,sessionStorage.getItem("authenticatedUser"))} 
 
 export default function ticket(props) {
 
