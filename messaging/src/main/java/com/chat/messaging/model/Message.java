3687700681dfa9e8ac2618a9b039ea471d6caf32
Aleksandar.G <425730@student.fontys.nl>
2021-01-09 11:07:57 +0100

    backedn for messaging

---
+++ messaging/src/main/java/com/chat/messaging/model/Message.java
@@ -0,0 +1,31 @@
+package com.chat.messaging.model;
+
+import java.sql.Date;
+
+public class Message {
+
+    private String Sender;
+    private String Message;
+    private Date Timestamp;
+
+    public Message() {
+    }
+
+    public Message(String sender, String message, Date timestamp) {
+        Sender = sender;
+        Message = message;
+        Timestamp = timestamp;
+    }
+
+    public String getSender() {
+        return Sender;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public Date getTimestamp() {
+        return Timestamp;
+    }
+}
